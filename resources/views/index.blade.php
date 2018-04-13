037c0a37840326cf593de7582044bc4fc37d4ee9
Floris Meininger <florismeininger@gmail.com>
2018-04-13 14:59:57 +0200

    lala

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,21 +19,54 @@
         var Sidebar = Backbone.Model.extend({
           promptColor: function() {
             var cssColor = prompt("Please enter a CSS color:");
-            this.set({color: cssColor});
+            var fles = prompt("enter fles status");
+            this.set({color: cssColor, fles: fles});
+          },
+          defaults: {
+            fiets:5000,
+            fles:"leeg",
+            bootlengte:2000000000
           }
         });
 
 
-        var object = new Sidebar();
-        object.promptColor();
+        var object = new Sidebar({bootlengte:34,fles:"vol"});
+         object.promptColor();
         console.log(object);
         showColor(object);
       }
 
       function showColor(object){
-        console.log(object.attributes.color);
+        console.log(object.get("fles"));
+        console.log(object.get("color"));
+        console.log(object.get("bootlengte"));
       }
+      function onOnderzoekTest(){
+        var Sidebar = Backbone.Model.extend({
+          promptColor: function() {
+            var cssColor = prompt("Please enter a CSS color:");
+            var fles = prompt("enter fles status");
+            this.set({color: cssColor, fles: fles});
+          },
+          defaults: {
+            fiets:5000,
+            fles:"leeg",
+            bootlengte:2000000000
+          }
+        });
 
+        var pannenkoek = new Sidebar();
+        pannenkoek.on(
+          'alert', function(msg){
+            alert('trigggert ' + msg);
+          },
+          'test', function(msg){
+            alert('pannenkoekje ' + msg);
+          }
+        )
+        pannenkoek.trigger("test", "jojo ik ben je brobro");
+
+      }
     </script>
 
   </head>
@@ -41,6 +74,7 @@
   <body>
     <button onclick='aanMaken()'>Aanmaken</button>
     <button onclick='showColor()'>Show Color</button>
+    <button onclick='onOnderzoekTest()'>onOnderzoekTest</button>
   </body>
 
 </html>
